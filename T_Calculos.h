d885ec5d4cc5ae3863f5f07b0445f58af955c615
Eufalo <adrian.gallegosanchez@gmail.com>
2016-06-18 18:45:35 +0200

    Update T_Calculos.h

--- T_Calculos.h
+++ T_Calculos.h
@@ -1,18 +1,25 @@
-#ifndef T_CALCULOS_H
-#define T_CALCULOS_H
-
+//
+//  T_Calculos.h
+//  insertar calculos
+//
+//  Created by Adrian on 18/6/16.
+//  Copyright © 2016 Adrian. All rights reserved.
+//
+#ifndef T_Calculos_H
+#define T_Calculos_H
+#include <iostream>
+#include <vector>
 
 class T_Calculos
 {
-    public:
-        T_Calculos(void);
-        void  Borrar_Calculo(void);
-        bool Insertar_Calculo(long double Nw_Calculo);
-        long double Buscar_Calculo(int Pos);
-    protected:
-
-    private:
-        std::vector<long double> Calculos_realizados;
+public:
+    T_Calculos(void);
+    void  Borrar_Calculo(void);
+    bool Insertar_Calculo(const long double &Nw_Calculo);
+    long double Buscar_Calculo(const int &Pos);
+protected:
+    
+private:
+    std::vector<long double> Calculos_realizados;
 };
-
-#endif // T_CALCULOS_H
+ // T_CALCULOS_H
